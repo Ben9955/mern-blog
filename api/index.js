4f0ae4b7f8d59baca74841372793f9c05382dc02
ben9955 <ben95ansari@gmail.com>
2024-02-23 19:14:52 +0000

    added comment section to the post page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import cookieParsel from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ app.listen(3000, () => console.log("Listening on port 3000"));
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 // creating error middleware
 app.use((err, req, res, next) => {
---
+++ api/models/commetn.model.js
@@ -0,0 +1,32 @@
+import mongoose, { mongo } from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+
+    numberOfLikes: {
+      type: Number,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
