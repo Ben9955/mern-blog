bb7b8bd55f01de4cab44e3febc4c2b722577e716
ben9955 <ben95ansari@gmail.com>
2024-04-08 18:29:22 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,12 +7,16 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 
+import path from "path";
+
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("MongoDb is connected"))
   .catch((err) => console.log(err));
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -27,6 +31,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 // creating error middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
