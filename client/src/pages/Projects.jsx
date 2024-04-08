2fa6780bb3eb23f4a7c7d9a42267a72a4f3a53d2
ben9955 <ben95ansari@gmail.com>
2024-04-08 18:10:27 +0100

    completed about and projects pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import CallToAction from "../components/CallToAction";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto gap-6 flex flex-col p-3">
+      <h1 className="text-3xl font-semibold ">Projects</h1>
+      <p>
+        Build fun and engaging projects while learning HTML, CSS, and
+        JavaScript!
+      </p>
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
